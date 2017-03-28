4127390bc2b38ada73f8b6af3d6e11c8e0b0773d
baoqger <baoqger@aliyun.com>
2017-03-28 09:17:31 +0800

    update the docs folder

--- docs/script/component/component-leftsidebar.js
+++
@@ -1,7 +0,0 @@
-(function(){
-    angular
-        .module("app")
-        .component("leftSidebar",{
-            templateUrl:"view/component-template/component-leftsidebar.html"
-        })
-})();
